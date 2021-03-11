b02b5655b3d70005da086f0a14db1d2039647590
Jonathan Schneider <globalatmo@gmail.com>
2021-03-11 16:31:10 +0100

    info

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -6,6 +6,8 @@
       @wheel="wheel"
       class="red"
   >
+    <img src="../assets/Seite_Info.png" id="info">
+    <div id="line"></div>
     <img src="../assets/Logo.png" id="logo">
     <slot></slot>
   </div>
@@ -59,9 +61,9 @@ export default {
 <style scoped>
 #logo {
   position: absolute;
-  top: 18vh;
-  left:10vw;
-  width: 73vw;
+  top: 12vh;
+  left:0vw;
+  width: 60vw;
 }
 #background{
   position: absolute;
@@ -70,6 +72,21 @@ export default {
   height: 100vh;
   overflow: auto;
   background: #3E2A33;
+}
+#line {
+  background: blue;
+  position: absolute;
+  top: 16.5px;
+  left: 115px;
+  width: 400vw;
+  height: 2px;
+
+}
+#info {
+  position: absolute;
+  top: 10px;
+  width: 115px;
+
 }
 @media all and (max-width: 1000px) {
   #background {
