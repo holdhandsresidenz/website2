97cd36aec4f5226d4f08c64e404e30c7ea039ae2
Jonathan Schneider <globalatmo@gmail.com>
2021-03-30 00:43:44 +0200

    montag abend bei reiner

--- src/pages/private/components/PostEdit.vue
+++ src/pages/private/components/PostEdit.vue
@@ -1,15 +1,40 @@
 <template>
   <div>
-    <div v-if="post.category === null" class="newPost"></div>
-    <div v-if="post.category !== null" class="editPost"></div>
+    <div v-if="post.category === null" class="newPost">
+      NEUEN POST ERSTELLEN
+      <select v-model="selectedCategory">
+        <option disabled value="">Bitte eine Kategorie auswählen</option>
+        <option>Text</option>
+        <option>Sound</option>
+        <option>Bilder</option>
+        <option>Video</option>
+      </select>
+      <PostEditSound v-if="selectedCategory === 'Sound'"></PostEditSound>
+      <PostEditImage v-if="selectedCategory === 'Bilder'"></PostEditImage>
+      <PostEditVideo v-if="selectedCategory === 'Video'"></PostEditVideo>
+      <PostEditTextLong v-if="selectedCategory === 'Text'"></PostEditTextLong>
+    </div>
+    <div v-if="post.category !== null" class="editPost">POST BEARBEITEN</div>
   </div>
 </template>
 
 <script>
+import PostEditTextLong from "./PostEditLongText";
+import PostEditVideo from "./PostEditVideo";
+import PostEditImage from "./PostEditImage";
+import PostEditSound from "./PostEditImage";
 export default {
   name: "PostEdit",
+  components: {
+    PostEditTextLong,
+    PostEditVideo,
+    PostEditImage,
+    PostEditSound,
+  },
   data: function () {
-    return {};
+    return {
+      selectedCategory: null,
+    };
   },
   props: {
     post: Object,
--- src/pages/private/components/PostEditImage.vue
+++ src/pages/private/components/PostEditImage.vue
@@ -1,8 +1,17 @@
 <template>
-  <div>{{ post }}</div>
+  <div>
+    Bilder
+    <input
+      type="file"
+      accept="image/*"
+      @change="uploadImage($event)"
+      id="file-input"
+    />
+  </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "PostEdit",
   data: function () {
@@ -17,6 +26,26 @@ export default {
   props: {
     post: Object,
   },
+  methods: {
+    uploadImage(event) {
+      const URL = "resident1/";
+
+      let data = new FormData();
+      data.append("name", "my-picture");
+      data.append("file", event.target.files[0]);
+
+      let config = {
+        header: {
+          "Content-Type": "image/png",
+        },
+      };
+
+      axios.put(URL, data, config).then((response) => {
+        console.log("image upload response > ", response);
+      });
+    },
+  },
+
   mounted() {
     this.author = this.post.author;
     this.idposts = this.post.idposts;
